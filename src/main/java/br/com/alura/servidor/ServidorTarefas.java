ce4ae2db37d3d437942ef2b1c8390412c69fae0d
Luiz Felipe <lffm1994@gmail.com>
2023-02-13 01:44:09 -0300

    Thread pool

--- src/main/java/br/com/alura/servidor/ServidorTarefas.java
+++ src/main/java/br/com/alura/servidor/ServidorTarefas.java
@@ -1,8 +1,13 @@
 package br.com.alura.servidor;
 
+import br.com.alura.tarefas.DistribuirTarefas;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefas {
     public static void main(String[] args) throws IOException {
@@ -10,9 +15,16 @@ public class ServidorTarefas {
         System.out.println("--- Iniciando servidor ---");
         ServerSocket servidor = new ServerSocket(12345);
 
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
         while (true) {
             Socket socket = servidor.accept();
             System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+
+            DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
+            // Thread threadCliente = new Thread(distribuirTarefas);
+            // threadCliente.start();
+            threadPool.execute(distribuirTarefas);
         }
     }
 }
